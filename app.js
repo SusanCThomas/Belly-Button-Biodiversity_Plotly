5694d96ccdec4c19206e27e38ff126219973b0ab
Susan C Thomas <susancthomas@Susans-MBP.lan>
2021-02-24 01:15:51 -0500

    create horizontal bar chart

--- app.js
+++ app.js
@@ -15,3 +15,30 @@ d3.json("data/samples.json").then((incomingData) => {
     names.forEach((name) => {
         d3.select("#selDataset").append("option").text(name);
     });
+
+// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.     
+
+    // Initializes the page with a default plot
+    function init() {
+
+        // Setting the first test id number as the default for plotting
+        defaultTestId = data.samples.filter(sample => sample.id === "940")[0];
+        console.log(defaultTestId);
+
+        // * Use `sample_values` as the values for the bar chart.
+        defaultSampleValues = defaultTestId.sample_values;
+
+        // * Use `otu_ids` as the labels for the bar chart.
+        defaultotuids = defaultTestId.otu_ids;
+
+        // * Use `otu_labels` as the hovertext for the chart.
+        defaultotulabels = defaultTestId.otu_labels;
+
+        // The top 10 OTUs
+        sampleValues = defaultSampleValues.slice(0, 10).reverse();
+        sampleotuids = defaultotuids.slice(0, 10).reverse();
+        sampleotulabels = defaultotulabels.slice(0, 10).reverse();
+
+        console.log(sampleValues);
+        console.log(sampleotuids);
+        console.log(sampleotulabels);
