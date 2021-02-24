b46db523ea0d5838f8aa6e2a6e6ae87d8a331411
Susan C Thomas <susancthomas@Susans-MBP.lan>
2021-02-24 01:12:40 -0500

    updated app.js file

--- app.js
+++ app.js
@@ -4,3 +4,14 @@
 var data;
 d3.json("data/samples.json").then((incomingData) => {
     console.log(incomingData);
+
+    // From samples.json
+    data = incomingData;
+    console.log(data);
+
+    // Adding test subject id number to the drop down menus
+    var names = data.names;
+    // Selecting dropdown tag
+    names.forEach((name) => {
+        d3.select("#selDataset").append("option").text(name);
+    });
